5799684b694ae6d43f86cc1cfc884c56172f57ce
mamanimartinpablo <mamanimartinpablo@gmail.com>
2025-10-15 23:38:51 -0300

    pocker continuacion

--- src/ar/edu/unq/po2/tp6/pocker/PokerStatus.java
+++ src/ar/edu/unq/po2/tp6/pocker/PokerStatus.java
@@ -3,18 +3,24 @@ package ar.edu.unq.po2.tp6.pocker;
 import java.util.ArrayList;
 
 public class PokerStatus {
+	
+	private ArrayList<String> cartas;
 		
-public Boolean verificar(String c1,String c2,String c3,String c4, String c5) {
+public PokerStatus(String c1, String c2, String c3, String c4, String c5) {
+		super();
+		this.cartas = new ArrayList<String>();
+		this.cartas.add(c1);
+		this.cartas.add(c2);
+		this.cartas.add(c3);
+		this.cartas.add(c4);
+		this.cartas.add(c5);
+	}
+
+public Boolean esPocker() {
 		
-		String primerLetra = c1.length() == 2?c1.substring(0,1):c1.substring(0, 2);
-		int contador = 1;
-		ArrayList<String> lista = new ArrayList<String>();
-		lista.add(c2);
-		lista.add(c3);
-		lista.add(c4);
-		lista.add(c5);
-		
-		for (String string : lista) {
+		String primerLetra = this.cartas.get(0).length() == 2?this.cartas.get(0).substring(0,1):this.cartas.get(0).substring(0, 2);
+		int contador = 0;
+		for (String string : this.cartas) {
 			
 			if (string.length() == 2 ) {
 				if (string.substring(0,1).equals(primerLetra)) {
@@ -31,5 +37,72 @@ public Boolean verificar(String c1,String c2,String c3,String c4, String c5) {
 		}
 		return contador == 4;
 	}
+
+public String verificar() {
+	// TODO Auto-generated method stub
+	if (this.esPocker()) {
+		return "Poquer";
+	}
+	else {
+		if (this.esTrio()) {
+			return "Trio";
+		}
+		else {
+			if (this.esColor()) {
+				return "Color";
+			}else {
+				return "Nada";
+			}
+		}
+	}
+	
+}
+
+private boolean esColor() {
+	// TODO Auto-generated method stub
+	String palo = this.cartas.get(0).length() == 2 ?this.cartas.get(0).substring(1,2):this.cartas.get(0).substring(2,3);
+	int contador = 0;
+	System.out.println(this.cartas.get(0).substring(0, 1));
+	System.out.println(this.cartas.get(0).substring(1, 2));
+	
+	for (String string : this.cartas) {
+		
+		if (string.length() == 2 ) {
+			if (string.substring(1,2).equals(palo)) {
+				contador = contador + 1;
+			}
+		}else {
+			if(string.length() == 3) {
+				if (string.substring(2,3).equals(palo)) {
+				contador = contador + 1;
+				}
+			}
+		}
+		
+	}
+	return contador == 5;
+}
+
+private boolean esTrio() {
+	// TODO Auto-generated method stub
+	String primerLetra = this.cartas.get(0).length() == 2?this.cartas.get(0).substring(0,1):this.cartas.get(0).substring(0, 2);
+	int contador = 0;
+	for (String string : this.cartas) {
+		
+		if (string.length() == 2 ) {
+			if (string.substring(0,1).equals(primerLetra)) {
+				contador = contador + 1;
+			}
+		}else {
+			if(string.length() == 3) {
+				if (string.substring(0,2).equals(primerLetra)) {
+				contador = contador + 1;
+				}
+			}
+		}
+		
+	}
+	return contador == 3;
+}
 
 }
--- test/ar/edu/unq/po2/tp6/pocker/TestPokerStatus.java
+++ test/ar/edu/unq/po2/tp6/pocker/TestPokerStatus.java
@@ -7,41 +7,68 @@ import org.junit.jupiter.api.Test;
 
 class TestPokerStatus {
 	
-	PokerStatus jugada;
+	PokerStatus jugada1;
+	PokerStatus jugada2;
+	PokerStatus jugada3;
+	PokerStatus jugada4;
+	PokerStatus jugada5;
+	PokerStatus jugada6;
+	PokerStatus jugada7;
 
 	@BeforeEach
 	void setUp() throws Exception {
-		jugada = new PokerStatus();
+		jugada1 = new PokerStatus("2P", "10P", "2C", "2D", "2T");
+		jugada2 = new PokerStatus("QD", "3P", "QC", "QP", "QT");
+		jugada3 = new PokerStatus("10D", "10P", "2P", "10C", "10T");
+		jugada4 = new PokerStatus("QD", "3P", "5P", "4C", "6T");
+		jugada5 = new PokerStatus("4D", "4P", "5P", "10C", "3T");
+		jugada6 = new PokerStatus("6D", "3P", "5P", "6C", "6T");
+		jugada7 = new PokerStatus("6D", "3D", "5D", "10D", "2D");
 	}
 
 	@Test
 	void testVerificarJugadaCon4CartasIgualesConValor2OseaPoker() {
-		assertTrue(jugada.verificar("2P", "10P", "2C", "2D", "2T"));
+		assertEquals("Poquer",jugada1.verificar());
 	}
 	
 	@Test
 	void testVerificarJugadaCon4CartasIgualesConValorDeReinaDeDiamantesOseaPoker() {
-		assertTrue(jugada.verificar("QD", "3P", "QC", "QP", "QT"));
+		assertEquals("Poquer",jugada2.verificar());
 	}
 	
 	@Test
 	void testVerificarJugadaCon4CartasIgualesConValorDe10OseaPoker() {
-		assertTrue(jugada.verificar("10D", "10P", "2P", "10C", "10T"));
+		assertEquals("Poquer",jugada3.verificar());
 	}
 	
 	@Test
-	void testVerificarJugadaConCartasDiferentesNoDePoker() {
-		assertFalse(jugada.verificar("QD", "3P", "5P", "4C", "6T"));
+	void testVerificarJugadaConCartasDiferentesDaNada() {
+		assertEquals("Nada",jugada4.verificar());
 	}
 	
 	@Test
-	void testVerificarJugadaCon2CartasIgualesEnSuValorNoDaPoker() {
-		assertFalse(jugada.verificar("4D", "4P", "5P", "10C", "3T"));
+	void testVerificarJugadaCon2CartasIgualesEnSuValorDaNada() {
+		assertEquals("Nada",jugada5.verificar());
 	}
 	
 	@Test
-	void testVerificarJugadaCon3CartasIgualesEnSuValorNoDaPoker() {
-		assertFalse(jugada.verificar("6D", "3P", "5P", "6C", "6T"));
+	void testVerificarJugadaCon3CartasIgualesEnSuValorDaTrio() {
+		assertEquals("Trio",jugada6.verificar());
+	}
+	
+	@Test
+	void testVerificarJugadaCon2CartasIgualesEnSuValorNoDaTrio() {
+		assertEquals("Nada",jugada5.verificar());
+	}
+	
+	@Test
+	void testVerificarJugadaConCartasDiferentesNoDaTrio() {
+		assertEquals("Nada",jugada5.verificar());
+	}
+	
+	@Test
+	void testVerificarJugadaCon5CartasDeIguaPaloEsColor() {
+		assertEquals("Color",jugada7.verificar());
 	}
 
 }
